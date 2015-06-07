63aff5365aec564b3bb22afb3c36312e1c11719d
olivier-maurie <olivier.maurie@gmail.com>
2015-06-07 10:39:45 +0200

    ajout fichiers

---
+++ pages/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="../lib/css/custom.css" type="text/css"/>
+	<link href="http://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	
+	<!-- JS -->	
+	<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js"></script>
+	<script type="text/javascript" src="../lib/js/map.js"></script>
+</head>
+<body>
+	<header>
+		<div class="content">
+			<h1> Dev'test : API Google </h1>
+		</div>
+	</header>
+	
+	<div id="googleMap"></div>
+	<div class="content" onload="initialiser()">
+	</div>
+
+</body>
+</html>
